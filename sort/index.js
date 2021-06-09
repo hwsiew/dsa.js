332d0a4beb94266c05d97f93d93841f077103b1b
hwsiew <hw.siew.work@gmail.com>
2021-06-09 17:35:30 +0800

    Added index file for sort folder

---
+++ sort/index.js
@@ -0,0 +1,9 @@
+const bubbleSort = require('./bubbleSort');
+const insertionSort = require('./insertionSort');
+const selectionSort = require('./selectionSort');
+
+module.exports = {
+	bubbleSort,
+	insertionSort,
+	selectionSort
+}
