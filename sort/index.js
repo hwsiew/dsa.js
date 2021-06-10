45bbf50f1db826ebc39a466cde7b322016bd33a5
hwsiew <hw.siew.work@gmail.com>
2021-06-10 13:38:29 +0800

    Updated sort index file

--- sort/index.js
+++ sort/index.js
@@ -1,9 +1,13 @@
 const bubbleSort = require('./bubbleSort');
 const insertionSort = require('./insertionSort');
 const selectionSort = require('./selectionSort');
+const heapSort = require('./heapSort');
+const mergeSort = require('./mergeSort');
 
 module.exports = {
 	bubbleSort,
 	insertionSort,
-	selectionSort
+	selectionSort,
+	heapSort,
+	mergeSort
 }
