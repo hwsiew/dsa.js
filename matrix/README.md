8e2fa70457fb5d7c76f60db56159bc7bb7d92d33
hwsiew <hw.siew.work@gmail.com>
2021-06-10 17:42:35 +0800

    Added matrix readme and index file

---
+++ matrix/README.md
@@ -0,0 +1,4 @@
+# Matrix
+operation supported
+- transpose
+- flipY
---
+++ matrix/index.js
@@ -0,0 +1,7 @@
+const transpose = require('./transpose');
+const flipY = require('./flipY');
+
+module.exports = {
+	transpose,
+	flipY
+}
