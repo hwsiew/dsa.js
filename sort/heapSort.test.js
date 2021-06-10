22e28dee124f4c5d397381c78507da5d192a9e9e
hwsiew <hw.siew.work@gmail.com>
2021-06-10 13:42:39 +0800

    Added additional test cases for heap sort

--- sort/heapSort.test.js
+++ sort/heapSort.test.js
@@ -30,3 +30,13 @@ it('should sort an string array in descending order', function(){
 	let arr = Array.from('fagdjkecbhi');
 	expect(heapSort(arr,(a,b) => b>a)).toEqual(['a','b','c','d','e','f','g','h','i','j','k'].reverse());
 });
+
+it('should sort duplicated value', function(){
+	let arr = [4,3,1,7,5,5,6,7,2,6,9,8];
+	expect(heapSort(arr)).toEqual([1,2,3,4,5,5,6,6,7,7,8,9])
+});
+
+it('should sort negative value', function(){
+	let arr = [4,3,-1,-7,5,-5,6,7,2,6,9,8];
+	expect(heapSort(arr)).toEqual([-7,-5,-1,2,3,4,5,6,6,7,8,9])
+});
