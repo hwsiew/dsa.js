5680c1783791e035cb3dae6576f3bf0e32178682
hwsiew <hw.siew.work@gmail.com>
2021-07-21 17:51:45 +0800

    Remove deprecated sorting algo files

--- sort/quickSort.test.js
+++
@@ -1,8 +0,0 @@
-const { it, expect } = require('@jest/globals');
-const quickSort = require('./quickSort');
-
-it('should sort an array in ascending order', function(){
-	let arr = [4,6,3,2,1,9,7];
-	quickSort(arr);
-	expect(arr).toEqual([1,2,3,4,6,7,9]);
-});
