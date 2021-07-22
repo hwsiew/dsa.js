bd7be2744c300411279b5a8847cc9b8b322bd192
hwsiew <hw.siew.work@gmail.com>
2021-07-22 14:14:17 +0800

    Update queue documentation

--- src/ds/queue/index.ts
+++ src/ds/queue/index.ts
@@ -1,10 +1,29 @@
 import CircularQueue from "./circularQueue";
 import PriorityQueue from "./priorityQueue";
 
+/**
+ * ## Queue
+ * 
+ * First in first out (FIFO) linear data structure
+ * 
+ * ### Examples
+ * ```javascript
+ * const { Queue } = require('dsajs');
+ * // or
+ * // import { Queue } from 'dsajs';
+ * 
+ * let q = new Queue();
+ * ```  
+ */
 class Queue {
 
-	_: Array<any>;
+	private _: Array<any>;
 
+	/**
+	 * @constructor
+	 * @param arr optional array to initialize queue
+	 * @throws Will throw an error if the argument is provided and is not an arry 
+	 */
 	constructor(arr: Array<any> = []){
 
 		if(!Array.isArray(arr))
