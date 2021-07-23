4b4574a0e95014f619643a7b9dd5ba8c60e3aeda
hwsiew <hw.siew.work@gmail.com>
2021-07-23 21:32:37 +0800

    Update build files

--- dist/esm/ds/graph/index.js
+++ dist/esm/ds/graph/index.js
@@ -12,20 +12,41 @@ import { LinkedList } from "../..";
  * ```
  */
 var Graph = /** @class */ (function () {
+    /**
+     * @constructor
+     */
     function Graph() {
         this._ = new Map();
     }
+    /**
+     * Add a vertex to the graph
+     *
+     * @param v vertex
+     * @returns
+     */
     Graph.prototype.addVertex = function (v) {
         if (this._.has(v))
             return; // do nothing is vertex exist
         var list = new LinkedList();
         this._.set(v, list);
     };
+    /**
+     * Get a vertex if available
+     *
+     * @param vertex
+     * @returns undefined if not vertex found
+     */
     Graph.prototype.getVertex = function (vertex) {
         if (!this._.has(vertex))
             return undefined;
         return this._.get(vertex);
     };
+    /**
+     * Delete a vertex if available
+     *
+     * @param {*} v
+     * @returns {boolean} if vertex is deleted successfully
+     */
     Graph.prototype.deleteVertex = function (v) {
         if (!this._.has(v))
             return false;
@@ -36,18 +57,37 @@ var Graph = /** @class */ (function () {
         });
         return true;
     };
+    /**
+     * Add an edge between from and to
+     *
+     * @param {*} from need to match the type of vertex added
+     * @param {*} to need to match the type of vertex added
+     */
     Graph.prototype.addEdge = function (from, to) {
         if (!this._.has(from) || !this._.has(to))
             throw new Error('Vertex is not found');
         var list = this._.get(from);
         list.append(to);
     };
+    /**
+     * Remove an edge between from and to
+     *
+     * @param {*} from need to match the type of vertex added
+     * @param {*} to need to match the type of vertex added
+     */
     Graph.prototype.removeEdge = function (from, to) {
         if (!this._.has(from) || !this._.has(to))
             throw new Error('Vertex is not found');
         var list = this._.get(from);
         list.delete(function (x) { return x === to; });
     };
+    /**
+     * Check if there exists a path between from and to
+     *
+     * @param {*} from
+     * @param {*} to
+     * @returns {boolean}
+     */
     Graph.prototype.hasPath = function (from, to) {
         if (!this._.has(from) || !this._.has(to))
             return false;
