0bc101e3cbb11d7f99dc168f8916604e53fc9b9d
hwsiew <hw.siew.work@gmail.com>
2021-07-26 22:22:05 +0800

    Add compare function type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,9 +1,9 @@
 /**
  * @param {*} a an element
  * @returns {number} 
- * * 0, when 'a' is the target
- * * -1 or <0, when 'a' is smaller than 'a'
- * * 1 or >0, when 'a' is larger than 'a'  
+ * * return 0, when 'a' is the target
+ * * return -1 or <0, when 'a' is smaller than 'a'
+ * * return 1 or >0, when 'a' is larger than 'a'  
  */
 export type BinarySearchCompareFunction = (a: any) => number;
 
@@ -13,3 +13,13 @@ export type BinarySearchCompareFunction = (a: any) => number;
  * @returns {boolean} true if 'b' should come before 'a' otherwise false
  */
 export type CompareFunctionBoolean = (a: any, b: any) => boolean;
+
+/**
+ * @param {*} a current element
+ * @param {*} b next element to compare
+ * @returns {number} 
+ * * return 0, when a == b
+ * * return <0, when b should be before a
+ * * return >0, when a should be before b
+ */
+export type CompareFunctionNumber = (a: any, b:any ) => number;
